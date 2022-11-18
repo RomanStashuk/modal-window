e087994d25b5a9dc76218363253b0ccca6c932c9
RomanStashuk <romanstashuk1@gmail.com>
2022-11-18 17:38:43 +0200

    add closing modal window functionality

--- src/index.js
+++ src/index.js
@@ -9,7 +9,24 @@ for (let btnShowModalWindow of btnsShowModalWindow) {
   btnShowModalWindow.addEventListener('click', showModalWindow);
 }
 
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+overlay.addEventListener('click', closeModalWindow);
+
+document.addEventListener('keydown', (evt) => {
+  if (
+    evt.key === 'Escape' &&
+    !modalWindow.classList.contains('visually-hidden')
+  ) {
+    closeModalWindow();
+  }
+});
+
 function showModalWindow() {
   modalWindow.classList.remove('visually-hidden');
   overlay.classList.remove('visually-hidden');
+}
+
+function closeModalWindow() {
+  modalWindow.classList.add('visually-hidden');
+  overlay.classList.add('visually-hidden');
 }
